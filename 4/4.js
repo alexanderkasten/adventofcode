3eb9e342adf2d58332cfca4d4352676fdbed4bc5
Alex <alexander.kasten@5minds.de>
2017-12-08 16:20:52 +0100

    :construction: WIP part one of day four

---
+++ 4/4.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt').toString();
+
+function start(){
+  let lines = input.split('\n');
+  let linearray;
+
+  let breakpoint;
+  let count = 0;
+
+  for(let a = 0; a< lines.length; a++){
+    linearray = lines[a].split(' ');
+    
+    for(let i = 0; i< linearray.length; i++){
+      for(let j = 0; j< linearray.length; j++){
+        if(linearray[i] === linearray[j] && i != j){
+          console.log('not valid');
+          breakpoint = true;
+          count = count+1;
+        }else{
+          console.log('valid');
+        }
+      }
+      if(breakpoint == true){
+        break;
+      }
+    }
+  console.log(linearray);
+  
+  }
+
+    
+  // console.log(lines[0]);
+  console.log(count);
+}
+
+start();
