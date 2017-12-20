6bbaffbd40383c480a634d353662c0b352d8d2cb
Alex <alexander.kasten@5minds.de>
2017-12-20 13:54:48 +0100

    :sparkles: Add day 4

--- 4/4.js
+++ 4/4.js
@@ -7,33 +7,20 @@ function start(){
   let lines = input.split('\n');
   let linearray;
 
-  let breakpoint;
   let count = 0;
 
   for(let a = 0; a< lines.length; a++){
     linearray = lines[a].split(' ');
-    
     for(let i = 0; i< linearray.length; i++){
       for(let j = 0; j< linearray.length; j++){
-        if(linearray[i] === linearray[j] && i != j){
-          console.log('not valid');
-          breakpoint = true;
+        if(linearray[i] === linearray[j] && i !== j){
           count = count+1;
-        }else{
-          console.log('valid');
+          i=linearray.length;
         }
       }
-      if(breakpoint == true){
-        break;
-      }
     }
-  console.log(linearray);
-  
   }
-
-    
-  // console.log(lines[0]);
-  console.log(count);
+  console.log(lines.length-count);
 }
 
 start();
