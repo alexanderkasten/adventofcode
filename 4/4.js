9ca57d4c6156e2474e6b44ba892d67ae61e62546
Alex <alexander.kasten@5minds.de>
2017-12-20 16:59:23 +0100

    :construction: WIP part 2 day 4

--- 4/4.js
+++ 4/4.js
@@ -3,11 +3,11 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString();
 
-function start(){
-  let lines = input.split('\n');
-  let linearray;
+let count = 0;
+let lines = input.split('\n');
 
-  let count = 0;
+function part1(){
+  let linearray;
 
   for(let a = 0; a< lines.length; a++){
     linearray = lines[a].split(' ');
@@ -21,6 +21,32 @@ function start(){
     }
   }
   console.log(lines.length-count);
+  part2();
 }
 
-start();
+function part2(){
+  let linearray;
+
+  count = 0;
+  
+  for(let a = 0; a< lines.length; a++){
+    linearray = lines[a].split(' ');
+    for(let i = 0; i< linearray.length; i++){
+      for(let j = 0; j< linearray.length; j++){
+        let sortedI = sortAlphabets(linearray[i]);
+        let sortedJ = sortAlphabets(linearray[j]);
+        if(sortedI === sortedJ && i !== j){
+          count = count+1;
+          i=linearray.length;
+        }
+      }
+    }
+  }
+  console.log(lines.length-count);
+}
+
+var sortAlphabets = function (text) {
+  return text.split('').sort().join('');
+};
+
+part1();
